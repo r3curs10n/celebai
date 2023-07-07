f6e5640364620fec454fc8325022cb075c16afd2
Shreyas Basarge <shreyasrulez@gmail.com>
2023-07-07 08:25:07 +0000

    lora inference

--- inference_lora.py
+++ inference_lora.py
@@ -1,17 +1,22 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
 import transformers
 import torch
 
-model = "tiiuae/falcon-7b"
+model_base = "tiiuae/falcon-7b"
+model_id = "./results/checkpoint-300"
 
-tokenizer = AutoTokenizer.from_pretrained(model)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_base, device_map={"":0})
+model = PeftModel.from_pretrained(model=model, model_id=model_id, is_trainable=False, device_map={"":0})
+
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
     torch_dtype=torch.bfloat16,
     trust_remote_code=True,
-    device_map="auto",
+    device_map={"": 0},
 )
 
 for i in range(5):
@@ -25,3 +30,4 @@ for i in range(5):
     )
     for seq in sequences:
         print(f"Result: {seq['generated_text']}")
+
